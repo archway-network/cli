ce02c7bea8c499ca8ebeac2ff246210bbbd0f3bc
Drew Taylor <drew@infiniteinternet.ca>
2021-09-16 20:37:53 -0400

    archway configure -m (modify) working

--- util/configure.js
+++ util/configure.js
@@ -1,13 +1,8 @@
 // archway-cli/util/configure.js
 
-const { spawn } = require("child_process");
+// const { spawn } = require("child_process");
 const FileSystem = require('fs');
 const StringUtility = require('util');
-
-// const readline = require('readline').createInterface({
-//   input: process.stdin,
-//   output: process.stdout
-// });
 
 function printDevConfig() {
   console.log('Printing environment settings...');
@@ -15,7 +10,7 @@ function printDevConfig() {
   let configPath = process.cwd() + '/config.json';
   FileSystem.access(configPath, FileSystem.F_OK, (err) => {
     if (err) {
-      console.error('Error locating dApp config at path ' + configPath + '. Please run this command from the root folder of valid Archway project.');
+      console.error('Error locating dApp config at path ' + configPath + '.\nPlease run this command from the root folder of valid Archway project.');
       return;
     } else {
       let config = require(configPath);
@@ -24,10 +19,78 @@ function printDevConfig() {
   })
 }
 
+function doCreateConfigFile(config = null) {
+  if (!config) {
+    console.log('Error creating config file', config);
+  } else if (typeof config !== 'object') {
+    console.log('Error creating config file', config);
+  } else if (!config.title || !config.version || !config.network || !config.path || !config.type) {
+    console.log('Error creating config file', config);
+  }
+
+  let path = config.path + '/config.json';
+  let json = JSON.stringify(config, null, 2);
+
+  FileSystem.writeFile(path, json, (err) => {
+    if (err)
+      console.log('Error writing config to file system', [config, err]);
+    else {
+      console.log('Successfully updated config file: ' + path + '\n');
+    }
+  });
+}
+
+function modifyConfig(key = null) {
+  if (!key || typeof key !== 'string') {
+    console.log(`Key "${key}" not found in config`);
+    return;
+  } else {
+    let configPath = process.cwd() + '/config.json', config;
+    FileSystem.access(configPath, FileSystem.F_OK, (err) => {
+      if (err) {
+        console.error('Error locating dApp config at path ' + configPath + '.\nPlease run this command from the root folder of valid Archway project.');
+        return;
+      } else {
+        config = require(configPath);
+        const readline = require('readline').createInterface({
+          input: process.stdin,
+          output: process.stdout
+        });
+
+        if (!config[key]) {
+          console.error(`${key} does not exist in config`);
+          readline.question('Print current config? (Y/N default: N): ', doPrintConfig => {
+            if (doPrintConfig.toLowerCase() !== 'y' && doPrintConfig.toLowerCase() !== 'yes') {
+              readline.close();
+              return;
+            } else {
+              printDevConfig();
+              readline.close();
+              return;
+            }
+          });
+        } else {
+          let currentValue = config[key];
+          console.log('The current value of ' + key + ' is: ', StringUtility.inspect(currentValue, false, null, true));
+          readline.question('Enter new value (ctrl+c to quit): ', newValue => {
+            let value = newValue.trim();
+            if (value.indexOf('{') > -1 && value.indexOf('}') > -1) {
+              value = JSON.parse(value);
+            }
+            config[key] = value;
+            doCreateConfigFile(config);
+            readline.close();
+            return;
+          });
+        }
+      }
+    });
+  }
+}
+
 const configureEnv = (modify = false, key = null) => {
   if (modify) {
-    console.log('Modifying key ' + key + '...');
-    console.log('XXX TODO: modify config');
+    modifyConfig(key);
   } else {
     printDevConfig();
   }
--- util/network.js
+++ util/network.js
@@ -1,1 +1,76 @@
 // archway-cli/util/network.js
+
+// const { spawn } = require("child_process");
+const FileSystem = require('fs');
+
+function migrateNetworks(networkInt = null) {
+  console.log('Network migration is coming soon');
+  console.log('XXX TODO: This')
+};
+
+function printNetConfig() {
+  console.log('Printing network settings...');
+
+  let configPath = process.cwd() + '/config.json';
+  FileSystem.access(configPath, FileSystem.F_OK, (err) => {
+    if (err) {
+      console.error('Error locating dApp config at path ' + configPath + '. Please run this command from the root folder of valid Archway project.');
+      return;
+    } else {
+      let config = require(configPath);
+      let chainId = config.network.chainId, 
+          rpc = config.network.urls.rpc.url + ':' + config.network.urls.rpc.port;
+          faucet = String(config.network.urls.rpc.faucets),
+          networks = ['  1. Testnet', '  2. Localhost', '  3. Mainnet'],
+          currentNetwork;
+
+      switch (chainId) {
+        case 'pebblenet-1': {
+          networks[0] += '*';
+          currentNetwork = networks[0];
+          break;
+        }
+        case 'localhost': {
+          networks[1] += '*';
+          currentNetwork = networks[1];
+          break;
+        }
+        case 'mainnet': {
+          networks[2] += '*';
+          currentNetwork = networks[2];
+        }
+      }
+
+      let networksMsg = networks.join('\n');
+      console.log('\n\n',networksMsg,'\n\n');
+      console.log('Using: ' + currentNetwork.replace('*','') + '\n');
+      console.log('RPC: ' + rpc + '\n');
+      console.log('Faucet: ' + faucet + '\n');
+
+      const readline = require('readline').createInterface({
+        input: process.stdin,
+        output: process.stdout
+      });
+
+      readline.question('Migrate to another network? (Y/N default: N): ', doMigration => {
+        if (doMigration.toLowerCase() !== 'y' && doMigration.toLowerCase() !== 'yes') {
+          console.log('Ok!');
+          readline.close();
+          return;
+        } else {
+          console.log('\n1. Testnet\n2. Localhost\n3. Mainnet\n');
+          readline.question('Select environment type (1-3 default: 1): ', environment => {
+            envI = parseInt(environment);
+            if (envI !== 1 && envI !== 2 && envI !== 3) {
+              console.log('Error: unrecognized environment', environment);
+              readline.close();
+            } else {
+              readline.close();
+              migrateNetworks(envI);
+            }
+          });
+        }
+      });
+    }
+  });
+}
