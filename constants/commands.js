b08ba68c891734757d8874b5373edfcb002fd486
Augusto Elesbão <aelesbao@gmail.com>
2021-12-07 12:51:48 +0100

    Remove stopped container after running the command

--- constants/commands.js
+++ constants/commands.js
@@ -2,7 +2,7 @@ module.exports = {
     ArchwayDocker: {
         cmd: 'docker',
         localDir: '/var/tmp/.archwayd',
-        args: ['run', '-it', '--volume=/var/tmp/.archwayd:/root/.archway', 'archwaynetwork/archwayd:latest']
+        args: ['run', '--rm', '-it', '--volume=/var/tmp/.archwayd:/root/.archway', 'archwaynetwork/archwayd:latest']
     },
     ArchwayBin: {
         cmd: 'archwayd',
