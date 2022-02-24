b71e37a6f89036d8730030b180c0761317486baa
Augusto Elesbão <aelesbao@gmail.com>
2022-02-24 16:00:21 +0100

    Utility modules

---
+++ src/util/config.js
@@ -0,0 +1,43 @@
+const path = require('path');
+const { writeFile } = require('fs/promises');
+const { spawn } = require('promisify-child-process');
+
+const ConfigFilename = 'config.json';
+
+async function findConfigRootPath() {
+  const { stdout } = await spawn(
+    'cargo',
+    ['locate-project', '--message-format', 'plain'],
+    { stdio: ['inherit', 'pipe', 'pipe'], encoding: 'utf8' }
+  );
+
+  const cargoFilePath = stdout ? stdout.toString() : undefined;
+  return path.dirname(cargoFilePath);
+}
+
+async function getConfigPath(pathPrefix = null) {
+  const rootPath = pathPrefix || await findConfigRootPath();
+  return path.join(rootPath, ConfigFilename);
+}
+
+async function writeConfig(config, pathPrefix = null) {
+  const configPath = await getConfigPath(pathPrefix);
+  const json = JSON.stringify(config, null, 2);
+  await writeFile(configPath, json);
+}
+
+async function readConfig(pathPrefix = null) {
+  try {
+    const configPath = await getConfigPath(pathPrefix);
+    return require(configPath);
+  } catch (e) {
+    throw new Error(`Failed to open the project config file ${ConfigFilename}: ${e.message || e}`);
+  }
+}
+
+module.exports = {
+  ConfigFilename,
+  read: readConfig,
+  write: writeConfig,
+  path: getConfigPath
+};
