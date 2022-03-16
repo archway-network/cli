dae96f1b6308ab3dbe2721e8214f83053cf53497
Augusto Elesbão <aelesbao@gmail.com>
2022-03-16 12:56:39 +0100

    Allow migrating networks using cli arguments

--- src/index.js
+++ src/index.js
@@ -175,8 +175,11 @@ Program
 Program
   .command('network')
   .description('Show network settings or migrate between networks')
-  .action(async () => {
-    await Tools.Network();
+  .addOption(new Option('-m, --migrate', 'Migrates the project to another network'))
+  .addOption(new Option('-e, --environment <value>', 'Environment to use for the project').choices(Environments))
+  .addOption(new Option('-t, --testnet <value>', 'Testnet to use for the project').choices(Testnets))
+  .action(async (options) => {
+    await Tools.Network(options);
   });
 
 // `archway new`
