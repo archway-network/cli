35990ee1398dbbb79ae120f7f449432b6906d8e3
Georges Chouchani <gachouchani@ndu.edu.lb>
2022-06-21 13:46:05 +0300

    query smart contract test added

--- src/clients/archwayd/__test__/query.test.js
+++ src/clients/archwayd/__test__/query.test.js
@@ -89,4 +89,25 @@ describe('QueryCommands', () => {
       });
     });
   });
+
+  describe('smartContract', () => {
+    test('queries a smart contract for a response', async () => {
+      const client = new ArchwayClient();
+      const query = new QueryCommands(client);
+
+      const archwayd = spawk.spawn(client.command)
+        .stdout(`Querying smart contract`);
+
+      const output = await query.smartContract("archway1dfxl39mvqlufzsdf089u4ltlhns6scgun6vf5mkym7cy0zpsrausequkm4", "{'get_count': {}}", defaultOptions);
+      console.log(output)
+      expect(output);
+      const args = "{'get_count': {}}"
+      expect(archwayd.calledWith).toMatchObject({
+        args: [
+          'query', 'wasm', 'contract-state', 'smart', 'archway1dfxl39mvqlufzsdf089u4ltlhns6scgun6vf5mkym7cy0zpsrausequkm4', args,
+          '--node', defaultOptions.node
+        ],
+      });
+    });
+  });
 });
