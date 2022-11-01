fdcb8265f181684fca457c0a20d9bcf33e8bc946
Augusto Elesbão <aelesbao@users.noreply.github.com>
2022-11-01 23:48:48 +0100

    feat: use the minimum gas fee in all transactions (#120)
    * feat: query rewards estimate-fees
    
    * refactor: query minimum gas price before all tx
    
    * fix: use fallback param for gas prices
    
    * fix: don't forward tx flags to query command
    
    * fix: update all testnet settings

--- src/clients/archwayd/tx.js
+++ src/clients/archwayd/tx.js
@@ -5,12 +5,9 @@ class TxCommands {
     this.#client = client;
   }
 
-  async wasm(wasmCommand, wasmArgs, { gas, ...options }) {
+  async wasm(wasmCommand, wasmArgs, options) {
     return await this.#run([
       'wasm', wasmCommand, ...wasmArgs,
-      '--gas', gas.mode,
-      '--gas-prices', gas.prices,
-      '--gas-adjustment', gas.adjustment,
     ], options);
   }
 
@@ -21,16 +18,35 @@ class TxCommands {
     ], options);
   }
 
-  async #run(txArgs = [], { from, chainId, node, flags = [], printStdout } = {}) {
+  async #run(txArgs = [], { gas = {}, from, chainId, node, flags = [], printStdout } = {}) {
+    const gasFlags = await this.#getGasFlags(gas, { node });
     const args = [
       ...txArgs,
       '--from', from,
       '--chain-id', chainId,
       '--node', node,
       '--broadcast-mode', 'sync',
+      ...gasFlags,
       ...flags
     ];
     return await this.#client.runJson('tx', args, { stdio: ['inherit', 'pipe', 'inherit'], printStdout });
+  }
+
+  async #getGasFlags({ mode = 'auto', prices: defaultGasPrices, adjustment = 1.2 }, options) {
+    const gasPrices = await this.#getMinimumConsensusFee(options) || defaultGasPrices;
+    return [
+      '--gas', mode,
+      '--gas-prices', gasPrices,
+      '--gas-adjustment', adjustment,
+    ];
+  }
+
+  async #getMinimumConsensusFee(options) {
+    try {
+      return await this.#client.query.rewardsEstimateFees(1, { printStdout: false, ...options });
+    } catch (e) {
+      return null;
+    }
   }
 }
 
