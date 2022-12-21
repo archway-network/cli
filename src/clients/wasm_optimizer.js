5eacf69f9882479b5f64f43b93960a5df3717eae
Augusto Elesbão <aelesbao@users.noreply.github.com>
2022-12-21 19:28:26 +0100

    feat(build): wasm optimizer wrapper

---
+++ src/clients/wasm_optimizer.js
@@ -0,0 +1,89 @@
+const debug = require('debug')('wasm-optimizer');
+const { WritableStream } = require('node:stream/web');
+const path = require('node:path');
+const Docker = require('dockerode');
+
+/**
+ * Wraps the rust-optimizer docker image and executes the proper optimizer for single projects or workspaces.
+ */
+class WasmOptimizer {
+  static RustOptimizerImage = 'cosmwasm/rust-optimizer';
+  static WorkspaceOptimizerImage = 'cosmwasm/workspace-optimizer';
+  static Version = '0.12.10';
+
+  /**
+   * @type {Docker}
+   */
+  #docker;
+
+  /**
+   * @param {Docker.DockerOptions} options
+   */
+  constructor(options = {}) {
+    this.#docker = new Docker(options);
+  }
+
+  /**
+   * Runs a docker container to optimize the wasm file.
+   *
+   * @param {string} workspaceRoot
+   * @param {boolean} isWorkspace
+   * @returns {Promise<{ error, statusCode }>}
+   */
+  async run(workspaceRoot, isWorkspace = false) {
+    try {
+      await this.#docker.ping();
+    } catch (e) {
+      throw new Error('Docker is not running. Please start Docker and try again.');
+    }
+
+    const image = `${isWorkspace ? WasmOptimizer.WorkspaceOptimizerImage : WasmOptimizer.RustOptimizerImage}:${WasmOptimizer.Version}`;
+    const projectName = path.basename(workspaceRoot);
+    const createOptions = {
+      name: `${projectName}-optimizer`,
+      AttachStdin: true,
+      AttachStdout: true,
+      AttachStderr: true,
+      Tty: false,
+      Env: ['CARGO_TERM_COLOR=always'],
+      Volumes: {
+        '/code': {},
+        '/code/target': {},
+        '/root/.cache/sccache': {},
+        '/usr/local/cargo/registry': {},
+      },
+      HostConfig: {
+        AutoRemove: true,
+        Binds: [
+          `${workspaceRoot}:/code`,
+          `${projectName}_cache:/code/target`,
+          'cosmwasm_sccache:/root/.cache/sccache',
+          'registry_cache:/usr/local/cargo/registry',
+        ],
+      },
+    };
+    debug('run', image, [], createOptions);
+
+    const stdout = new WritableStream({
+      write(chunk) {
+        process.stdout.write(chunk);
+      },
+    });
+    const stderr = new WritableStream({
+      write(chunk) {
+        process.stderr.write(chunk);
+      },
+    });
+
+    const [{ Error: error, StatusCode: statusCode }] = await this.#docker.run(
+      image,
+      [],
+      [stdout.getWriter(), stderr.getWriter()],
+      createOptions
+    );
+
+    return { error, statusCode };
+  }
+}
+
+module.exports = WasmOptimizer;
