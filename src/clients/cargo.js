48fdeb2c4ca51a260a4184c6b2a9c7e38bf61a68
Augusto Elesbão <aelesbao@users.noreply.github.com>
2023-06-28 15:31:26 +0200

    fix(cargo): resolve the WASM file name on arm64 (#196)

--- src/clients/cargo.js
+++ src/clients/cargo.js
@@ -21,13 +21,7 @@ class Cargo {
   }
 
   async generate(name, repository, branch, template) {
-    await this.#run([
-      'generate',
-      '--name', name,
-      '--git', repository,
-      '--branch', branch,
-      template
-    ]);
+    await this.#run(['generate', '--name', name, '--git', repository, '--branch', branch, template]);
   }
 
   async build({ release = false, locked = false, target } = {}, options) {
@@ -50,12 +44,7 @@ class Cargo {
    * @private
    */
   async metadata() {
-    const { stdout } = await this.#run([
-      'metadata',
-      '--quiet',
-      '--no-deps',
-      '--format-version=1'
-    ], { stdio: 'pipe' });
+    const { stdout } = await this.#run(['metadata', '--quiet', '--no-deps', '--format-version=1'], { stdio: 'pipe' });
     return JSON.parse(stdout);
   }
 
@@ -70,13 +59,16 @@ class Cargo {
     const { packages = [], target_directory: targetDirectory, workspace_root: workspaceRoot } = await this.metadata();
     const currentManifestPath = await this.locateProject();
 
-    const findPackageInPath = searchPath => packages
-      .find(({ manifest_path: manifestPath }) => path.dirname(manifestPath) === searchPath);
+    const findPackageInPath = searchPath =>
+      packages.find(({ manifest_path: manifestPath }) => path.dirname(manifestPath) === searchPath);
 
-    const firstPackageInWorkspace = searchPath => workspaceRoot === searchPath ? _.head(packages) : null;
+    const firstPackageInWorkspace = searchPath => (workspaceRoot === searchPath ? _.head(packages) : null);
 
-    const { name, version, manifest_path: manifestPath } =
-      findPackageInPath(currentManifestPath) || firstPackageInWorkspace(currentManifestPath) || {};
+    const {
+      name,
+      version,
+      manifest_path: manifestPath,
+    } = findPackageInPath(currentManifestPath) || firstPackageInWorkspace(currentManifestPath) || {};
 
     if (_.isEmpty(name) || _.isEmpty(version)) {
       throw new Error('Failed to resolve project metadata');
@@ -84,11 +76,12 @@ class Cargo {
 
     const id = `${name} ${version}`;
     const isWorkspace = path.dirname(manifestPath) !== workspaceRoot;
-    const wasmFileName = `${name.replace(/-/g, '_')}.wasm`;
+    const archSuffix = process.arch === 'arm64' ? '-aarch64' : '';
+    const wasmFileName = `${name.replace(/-/g, '_')}${archSuffix}.wasm`;
     const wasm = {
       fileName: wasmFileName,
       filePath: path.join(targetDirectory, Cargo.WasmTarget, 'release', wasmFileName),
-      optimizedFilePath: path.join(workspaceRoot, 'artifacts', wasmFileName)
+      optimizedFilePath: path.join(workspaceRoot, 'artifacts', wasmFileName),
     };
 
     return { id, name, version, wasm, workspaceRoot, isWorkspace };
@@ -107,7 +100,7 @@ class Cargo {
       ...options,
       cwd: this.#cwd,
       encoding: 'utf8',
-      maxBuffer: 1024 * 1024 // (Large enough for verbose error debugging)
+      maxBuffer: 1024 * 1024, // (Large enough for verbose error debugging)
     });
   }
 }
