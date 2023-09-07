69c277a290a8dc8910a208b7b2b3ce504858762c
Augusto Elesbão <aelesbao@users.noreply.github.com>
2023-09-07 12:07:32 +0200

    chore: add scopes for the commit linter

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,7 @@
-module.exports = {extends: ['@commitlint/config-conventional']};
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'scope-enum': [2, 'always', ['accounts', 'config', 'contracts', 'new', 'plugins', 'rewards', 'ui']],
+    'scope-case': [2, 'always', 'kebab-case'],
+  },
+};
