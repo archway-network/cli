0734cb145f653102f36274ccff44cd03764d36f1
Augusto Elesbão <aelesbao@users.noreply.github.com>
2023-09-20 10:53:22 +0200

    chore: update commitlint scopes

--- commitlint.config.js
+++ commitlint.config.js
@@ -2,6 +2,9 @@ module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
     'scope-enum': [2, 'always', [
+      // This list should be kept in sync with the
+      // PR Lint workflow at .github/workflows/pr-lint.yml
+      'deps',
       'core',
       'new',
       'accounts',
